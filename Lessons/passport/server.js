a0852e6b41837a06ae398a73c8ca3d1e3e1325e1
Dani Roxberry <dani@bitoriented.com>
2019-02-20 12:48:36 -0800

    [add] passport lesson plan

--- Lessons/passport/server.js
+++ Lessons/passport/server.js
@@ -1,4 +1,4 @@
-require('dotenv').load();
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const passport = require("./config/passport");
